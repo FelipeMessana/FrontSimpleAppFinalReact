85e4dab9bdde21fec9e2196e61c1f9ab984d40f0
FeliMessana <felipemessana@epet12smandes.edu.ar>
2024-10-01 09:38:58 -0300

    Objetivo 3

--- src/config/urlapis.js
+++ src/config/urlapis.js
@@ -1,4 +1,4 @@
-export const url = "http://192.168.14.183:3000";
+export const url = "http://192.168.212.183:3000";
 export const login = `${url}/login`;
 export const signup = `${url}/user/usersp`;
 // ip casa abuela : 192.168.1.89
